1edaa21350ed789bdbb27bbce9cc0987ae1dcc80
abhijeet-pujhari <abhijeetpujhari@gmail.com>
2023-06-14 18:55:34 +0530

    Update main.py
    added command line args

--- simulation/main.py
+++ simulation/main.py
@@ -1,18 +1,18 @@
 import simpy
-import random
+import argparse
 from resources.config import conf
 from resources.named_resource import NamedResource
 from processes.simulation_processes import vessel_generator, berth
 
 
 
-def run_simulation(simulation_time):
+def run_simulation(simulation_time, quay_crane_arg, truck_resource_arg):
     # Create environment
     env = simpy.Environment()
 
     vessel_queue = simpy.Store(env)
-    quay_crane = NamedResource(env, capacity=2, name="Quay Crane")
-    truck_resource = NamedResource(env, capacity=3, name="Truck")
+    quay_crane = NamedResource(env, capacity=quay_crane_arg, name="Quay Crane")
+    truck_resource = NamedResource(env, capacity=truck_resource_arg, name="Truck")
     logs = []
 
     # Start vessel generator process
@@ -26,4 +26,27 @@ def run_simulation(simulation_time):
     env.run(until=simulation_time)
 
 # Run the simulation
-run_simulation(conf.get("SIMULATION_TIME"))
+# run_simulation(conf.get("SIMULATION_TIME"))
+def main():
+    # Create the argument parser
+    parser = argparse.ArgumentParser(description='Container Terminal Simulation')
+
+    # Add the 
+    parser.add_argument('--time', type=int, default=conf.get('SIMULATION_TIME'),
+                        help='time')
+    
+    parser.add_argument('--trucks', type=int, default=conf.get('NUM_TRUCKS'),
+                        help='number of trucks')
+    
+    parser.add_argument('--cranes', type=int, default=conf.get('NUM_QUAY_CRANES'),
+                        help='number of cranes')
+    
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Run the simulation
+    run_simulation(args.time, args.trucks, args.cranes)
+
+if __name__ == '__main__':
+    main()
